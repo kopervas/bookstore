5dd7a299a9c1dc3c15fcd01073dc986764f969d4
Serhii Kopervas <sergkopervas@gmail.com>
2025-05-09 15:39:00 +0200

    1.0.3 Fourth commit. Bug fixes according to comments from Liliia Ponomarenko.

--- src/main/java/com/bookstore/model/Book.java
+++ src/main/java/com/bookstore/model/Book.java
@@ -7,7 +7,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import java.math.BigDecimal;
 import lombok.Data;
-import lombok.NonNull;
 
 @Entity
 @Data
@@ -15,18 +14,13 @@ public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NonNull
+    @Column(nullable = false)
     private String title;
-    @NonNull
+    @Column(nullable = false)
     private String author;
-    @NonNull
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String isbn;
     private BigDecimal price;
     private String description;
     private String coverImage;
-
-    public Book() {
-
-    }
 }
--- src/main/java/com/bookstore/repository/impl/BookRepositoryImpl.java
+++ src/main/java/com/bookstore/repository/impl/BookRepositoryImpl.java
@@ -2,29 +2,50 @@ package com.bookstore.repository.impl;
 
 import com.bookstore.model.Book;
 import com.bookstore.repository.BookRepository;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
 import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class BookRepositoryImpl implements BookRepository {
-    @PersistenceContext
-    private EntityManager entityManager;
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public BookRepositoryImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
 
     @Override
     public Book save(Book book) {
-        entityManager.persist(book);
-        return book;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(book);
+            transaction.commit();
+            return book;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can`t insert book into DB: " + book);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List<Book> findAll() {
-        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
-        CriteriaQuery<Book> cq = cb.createQuery(Book.class);
-        cq.from(Book.class);
-        return entityManager.createQuery(cq).getResultList();
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("SELECT u FROM Book u", Book.class).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can`t get all books from DB", e);
+        }
     }
 }
