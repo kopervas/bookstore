1aa475ca946b440f680df49c3679cbeb3b89c1db
Serhii Kopervas <sergkopervas@gmail.com>
2025-09-08 20:27:50 +0200

    1.4.0 Implementation of GlobalExceptionHandler and Data Validation.

--- src/main/java/com/bookstore/model/Book.java
+++ src/main/java/com/bookstore/model/Book.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
 import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.Setter;
@@ -25,6 +26,7 @@ public class Book {
     @Column(unique = true, nullable = false)
     private String isbn;
     @Column(nullable = false)
+    @Min(0)
     private BigDecimal price;
     @Column
     private String description;
