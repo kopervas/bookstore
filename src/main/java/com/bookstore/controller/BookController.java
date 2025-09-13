64cecf9ca935d635dd485ee16c39b7ccf051b0d4
kopervas <sergkopervas@gmail.com>
2025-09-13 20:02:41 +0200

    Refactor (#9)
    * Revert "Revert "1.0.0 First commit.""
    
    This reverts commit 4871f167f807e6bbc0caa61e8b00c0b945f2ec05.
    
    * 1.0.1 Secont commit - check ci.yml.
    
    * 1.0.2 Third commit - created the first model Book, BookRepository and BookService.
    
    * 1.0.3 Fourth commit. Bug fixes according to comments from Liliia Ponomarenko.
    
    * 1.0.4 Fifth commit. Bug fixes according to comments from Olena Bruiako.
    
    * 1.0.5 Sixth commit. Bug fixes according to comments from Olena Bruiako.
    
    * 1.0.6 Seventh commit. Added Controller, DTO, implemented MapStruct.
    
    * 1.0.7 Revert commit. Undo all changes made to the code in commit 1.0.6..
    
    * Revert "1.0.7 Revert commit. Undo all changes made to the code in commit 1.0.6.."
    
    This reverts commit 3eef526d0e0f9e713e93c4409651cec40258587c.
    
    * Revert "Revert "1.0.0 First commit.""
    
    This reverts commit 4871f167f807e6bbc0caa61e8b00c0b945f2ec05.
    
    * 1.0.1 Secont commit - check ci.yml.
    
    * 1.0.2 Third commit - created the first model Book, BookRepository and BookService.
    
    * 1.0.3 Fourth commit. Bug fixes according to comments from Liliia Ponomarenko.
    
    * 1.0.4 Fifth commit. Bug fixes according to comments from Olena Bruiako.
    
    * 1.0.5 Sixth commit. Bug fixes according to comments from Olena Bruiako.
    
    * 1.0.6 Seventh commit. Added Controller, DTO, implemented MapStruct.
    
    * 1.0.7 Revert commit. Undo all changes made to the code in commit 1.0.6..
    
    * Revert "1.0.7 Revert commit. Undo all changes made to the code in commit 1.0.6.."
    
    This reverts commit 3eef526d0e0f9e713e93c4409651cec40258587c.
    
    * 1.0.8 Correction of errors according to comments from Olena Bruiako.
    
    * 1.0.9 Correction of errors according to comments from Liliia Ponomarenko.
    
    * 1.1.0 Correction of errors according to comments from Olena Bruiako.
    
    * 1.2.0 Release of the task to the topic 'Spring Data JPA'. Used Lombok, Liquibase, implemented soft delete concept for the main entity.
    
    * 1.2.0 Release of the task to the topic 'Spring Data JPA'. Used Lombok, Liquibase, implemented soft delete concept for the main entity.
    
    * 1.2.1 Release with bug fixes according to Olena Bruiako's comments.
    
    * 1.2.2 Second release with bug fixes according to Olena Bruiako's comments.
    
    * 1.2.3 Third release with bug fixes according to Olena Bruiako's comments.
    
    * 1.2.4 Release with bug fixes according to Olena Bruiako's comments.
     Was:
       spring.jpa.hibernate.ddl-auto=none
     Became:
       spring.jpa.hibernate.ddl-auto=validate
    
    * 1.3.0 An additional task to the chapter "Criteria Query in Spring Boot".
    
    * 1.3.1 Errors correction according to comments from Olena Bruiako from 29.08.2025.

---
+++ src/main/java/com/bookstore/controller/BookController.java
@@ -0,0 +1,52 @@
+package com.bookstore.controller;
+
+import com.bookstore.dto.BookDto;
+import com.bookstore.dto.BookSearchParameters;
+import com.bookstore.dto.CreateBookRequestDto;
+import com.bookstore.dto.UpdateBookRequestDto;
+import com.bookstore.service.BookService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/books")
+public class BookController {
+    private final BookService bookService;
+
+    @GetMapping
+    public List<BookDto> getAll() {
+        return bookService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public BookDto getBookById(@PathVariable Long id) {
+        return bookService.findById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public BookDto createBook(@RequestBody CreateBookRequestDto requestDto) {
+        return bookService.save(requestDto);
+    }
+
+    @PutMapping("/{id}")
+    public BookDto update(@PathVariable Long id,
+                          @RequestBody UpdateBookRequestDto requestDto) {
+        return bookService.update(id, requestDto);
+    }
+
+    @GetMapping("/search")
+    public List<BookDto> searchBooks(BookSearchParameters searchParameters) {
+        return bookService.search(searchParameters);
+    }
+}
--- src/main/java/com/bookstore/model/Book.java
+++ src/main/java/com/bookstore/model/Book.java
@@ -30,4 +30,6 @@ public class Book {
     private String description;
     @Column
     private String coverImage;
+    @Column(nullable = false)
+    private boolean isDeleted = false;
 }
